c9771510a1138fc942f6f3af0adf863e758e4e5f
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2026-09-15 15:50:47 +0000

    use post for home

--- config/app.php
+++ config/app.php
@@ -40,7 +40,7 @@ return [
 	// should be the content type name/type (e.g., `post`) set in the
 	// `/config/content.php` configuration file.  Leave empty to show the
 	// normal homepage.
-	'home_alias' => '',
+	'home_alias' => 'post',
 
     'primary' => [
         'Blog' => '/blog',
