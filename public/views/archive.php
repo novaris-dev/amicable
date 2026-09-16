f4a1d732736338c9db749767d8e61c2d61abe959
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2026-09-16 23:58:09 +0000

    no longer needed as the files are in public/views/content by default.

--- public/views/archive.php
+++
@@ -1,3 +0,0 @@
-<?php $engine->include( 'header.default' ) ?>
-<?php $engine->include( 'content.archive' ); ?>
-<?php $engine->include( 'footer.default' ) ?>
--- public/views/single.php
+++
@@ -1,3 +0,0 @@
-<?php $engine->include( 'header/default' ); ?>
-<?php $engine->include( 'content.single' ); ?>
-<?php $engine->include( 'footer/default' ); ?>
