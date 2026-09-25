291ed157b5dee5bcf3440a31429a5b3c0931c45a
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2026-09-25 22:28:37 +0000

    add thumbnail for feature image.

--- public/views/entry/collection.php
+++ public/views/entry/collection.php
@@ -1,14 +1,26 @@
-<article id="" class="<?= e( post_class() ) ?>">
-    <header class="entry-header">
-        <div class="entry-metadata">
-            <?= $entry->date(); ?>
-        </div>
-        <h2 class="entry-title"><a href="<?= e( $entry->url() ); ?>"><?= e( $entry->title() ); ?></a></h2>
-    </header>
-    <div class="entry-content">
-        <?= $entry->excerpt( 50, sprintf(
-            ' &hellip; <a class="entry-more-link" href="%s">Continue reading&nbsp;&rarr;</a>',
-            e( $entry->uri() )
-        ) ) ?>
-    </div>
+<article class="<?= e( post_class() ) ?>">
+	<header class="entry__header">
+		<div class="entry__metadata">
+			<?= $entry->date(); ?>
+		</div>
+
+		<h2 class="entry__title">
+			<a class="entry__title-anchor" href="<?= e( $entry->url() ); ?>">
+				<?= e( $entry->title() ); ?>
+			</a>
+		</h2>
+	</header>
+
+	<?php if ( $thumbnail = $entry->thumbnail( 'amicable-landscape-large' ) ) : ?>
+		<picture class="entry__thumbnail">
+			<img class="entry__thumbnail" src="<?= e( $thumbnail->url() ); ?>" width="<?= $thumbnail->width(); ?>" height="<?= $thumbnail->height(); ?>" alt="">
+		</picture>
+	<?php endif ?>
+
+	<div class="entry__content">
+		<?= $entry->excerpt( 50, sprintf(
+			' &hellip; <a class="entry__more-link" href="%s">Continue reading&nbsp;&rarr;</a>',
+			e( $entry->uri() )
+		) ) ?>
+	</div>
 </article>
