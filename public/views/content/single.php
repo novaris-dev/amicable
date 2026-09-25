960e4b5dc7ebf8d97fc47c103478364b44960beb
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2026-09-25 23:21:36 +0000

    change to BEM format

--- public/views/content/single.php
+++ public/views/content/single.php
@@ -1,27 +1,37 @@
 <section id="content" class="site-content">
 	<main id="main" class="content-area">
-		<article id="" class="<?= e( post_class() ) ?>">
-            <header class="entry-header">
-                <div class="entry-metadata">
-                    <?= $single->date(); ?>
-                </div>
-                <h1 class="entry-title"><?= e( $single->title() ); ?></h1>
-            </header>
-            <div class="entry-content">
-                <?= $single->content() ?>
-            </div>
-            <footer class="entry-footer">
-
-			<?php if ( $tags = $single->terms( 'category' ) ) : ?>
-				<div class="entry-terms">
-					Tagged:
-					<?php foreach ( $tags as $tag ) : ?>
-						<a href="<?= e( $tag->uri() ) ?>">#<?= e( $tag->name() ) ?></a>
-					<?php endforeach ?>
+		<article class="<?= e( post_class() ) ?>">
+			<header class="entry__header">
+				<div class="entry__metadata">
+					<?= $single->date(); ?>
 				</div>
+
+				<h1 class="entry__title">
+					<?= e( $single->title() ); ?>
+				</h1>
+			</header>
+			<?php if ( $thumbnail = $single->thumbnail( 'amicable-landscape-extra-large' ) ) : ?>
+				<picture class="entry__thumbnail">
+					<img class="entry__thumbnail" src="<?= e( $thumbnail->url() ); ?>" width="<?= $thumbnail->width(); ?>" height="<?= $thumbnail->height(); ?>" alt="">
+				</picture>
 			<?php endif ?>
-			<?= $engine->archives() ?>
-		</footer>
+			<div class="entry__content">
+				<?= $single->content() ?>
+			</div>
+
+			<footer class="entry__footer">
+				<?php if ( $tags = $single->terms( 'category' ) ) : ?>
+					<div class="entry__terms">
+						Tagged:
+
+						<?php foreach ( $tags as $tag ) : ?>
+							<a class="entry__term-anchor" href="<?= e( $tag->uri() ) ?>">
+								#<?= e( $tag->name() ) ?>
+							</a>
+						<?php endforeach ?>
+					</div>
+				<?php endif ?>
+			</footer>
 		</article>
 	</main>
 </section>
