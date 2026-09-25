273baa0dd75c9a3ca22e0429a419987411670e57
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2026-09-25 22:27:17 +0000

    add pagination

--- public/views/content/collection.php
+++ public/views/content/collection.php
@@ -1,5 +1,6 @@
 <section id="content" class="site-content">
 	<main id="main" class="content-area">
 		<?php $engine->each( 'entry.collection', $entries, 'entry' ); ?>
+		<?= $engine->includeWhen( $pagination, 'menu.pagination' ); ?>
 	</main>
 </section>
